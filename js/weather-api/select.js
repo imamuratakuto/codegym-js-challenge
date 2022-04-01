41e37a1285fd625debbfd3eba80df2fbc918a265
imamuratakuto <love.east32@gmail.com>
2022-04-01 06:09:54 +0000

    総合課題

--- js/weather-api/select.js
+++ js/weather-api/select.js
@@ -1,18 +1,21 @@
 let select = document.getElementById('selectCountry');
-console.log(select);
+const weathers = {"Clear": "晴れ", "Clouds": "くもり", "Rain": "雨", "Snow": "雪", "Mist": "霧"};
 const getUrl = () => {
         const index = select.selectedIndex;
         const selectCountry = select[index].value;
-        const url =  "https://api.openweathermap.org/data/2.5/weather?q="+ selectCountry +"&appid=4b5774e9f3d2a07b84f0f2f88e486224";
+        const url =  "https://api.openweathermap.org/data/2.5/weather?q="+ selectCountry +"&units=metric&appid=4b5774e9f3d2a07b84f0f2f88e486224";
         fetch(url)
             .then(response => response.json())
             .then(data => getWeather(data))
             .catch(error => console.log("失敗しました"));
-  
         const getWeather = (data) => {
-            const result_text = document.getElementById('result-text');
-            result_text.innerHTML = "天気 : " + data.weather[0]["main"] + "<br>" + "平均気温 : " + data.main["temp"] + "<br>" + "湿度 : " + data.main["humidity"] + "%";
-            };
+                    const weather_text = document.getElementById('weather');
+                    const temp_text = document.getElementById('temp');
+                    const humidity_text = document.getElementById('humidity');
+                    weather_text.innerHTML = weathers[data.weather[0]["main"]];
+                    temp_text.innerHTML = data.main["temp"] + "℃";
+                    humidity_text.innerHTML = data.main["humidity"] + "%";
+        };
 };
 window.addEventListener('load', getUrl);
 select.addEventListener('change', getUrl);
